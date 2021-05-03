e74a39687bf42315bc0db554e2b2746471d1ec80
stephchen <stephchen@wandb.com>
2021-05-03 12:08:34 -0700

    run resumes in practice, check for run.resumed

--- examples/pytorch/pytorch-cifar10-sagemaker/train.py
+++ examples/pytorch/pytorch-cifar10-sagemaker/train.py
@@ -45,6 +45,8 @@ except botocore.exceptions.ClientError as e:
         path='data', bucket=bucket, key_prefix=prefix)
 print("Using inputs: ", inputs)
 
+print(f'train side: resumed? {wandb.run.resumed}')
+
 estimator = PyTorch(entry_point="cifar10.py",
                     source_dir=os.getcwd() + "/source",
                     role=role,
